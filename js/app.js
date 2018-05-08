08e63f28ce19af5551f17f724d8277236b5bf589
Jonathan DiQuattro <jondiquattro@hotmail.com>
2018-05-08 07:58:26 -0700

    put the pikes place attributes into array of objects called salmonStore

--- js/app.js
+++ js/app.js
@@ -1,56 +1,162 @@
 'strict'
 
-var customers = [0];
 
 
-//get random number and store in an array to represent customers
+// var pikesPlace = {
 
-function ranNumGen(){
-    var getRan = Math.floor(Math.random() * (65 - 23 + 1)) + 23;
-   
-   return getRan;
-   }
-   
-   ranNumGen();
-   
+//     minimumCustomers: 23,
+//     maxCustomers: 65,
+//     averageCookies: 6.3,
+// //num generator
+//     ranNumGen: function(){
+//         return Math.floor(Math.random() * (65 - 23 + 1)) + 23;
+//        },
+// // stores hours in array
+    
+// customers: [],
+//     storeHours: function(){
+        
+//         for (var i = 0; i < 14; i++){
 
-   function storeHours(){
-    for (var i = 0; i < 14; i++){
-        customers[i] = ranNumGen();
+//             var ranNumber = this.ranNumGen();
+//             console.log(ranNumber);
+//             this.customers.push(ranNumber);
+                    
+//             }
+//        },
+
+//        //calculates cookies per hour
+//        cookiesPerHour: [],
+//        calcCookiesPerHour: function(){
+//         for(i=0; i < this.customers.length; i++){
+//         this.cookiesPerHour[i] = this.customers[i] * 6.3;
+//         }
+
+//         },//end function
+//         sumCookies: 0,
+//         calcTotalCookies: function(){
+//             for(i=0; i < this.cookiesPerHour.length; i++){
+//                 this.sumCookies = this.sumCookies + this.cookiesPerHour[i];
                 
+//                 }
+              
+//         }//end calctotal function
+// }
+
+// pikesPlace.storeHours();
+// pikesPlace.calcCookiesPerHour();
+// console.log(pikesPlace.cookiesPerHour);
+// pikesPlace.calcTotalCookies();
+// console.log(pikesPlace.sumCookies);
+// console.log(pikesPlace.cookiesPerHour);
+
+
+// ranNumGen();
+// storeHours();
+// calcCookiesPerHour();
+// calcTotalCookies();
+
+
+
+
+//make object containing pikesPlace
+// var cookieStores ={
+// salmonStores:[
+//     {
+//         store: 'Pikes Place',
+//         //actual pikes object so I can print total cookies and cookies per hour.
+var salmonStores = {
+    storeName:[
+        {
+name: 'Pikes Place',
+    minimumCustomers: 23,
+    maxCustomers: 65,
+    averageCookies: 6.3,
+//num generator
+    ranNumGen: function(){
+        return Math.floor(Math.random() * (65 - 23 + 1)) + 23;
+       },
+// stores hours in array
+    
+customers: [],
+    storeHours: function(){
+        
+        for (var i = 0; i < 14; i++){
+
+            var ranNumber = this.ranNumGen();
+            console.log(ranNumber);
+            this.customers.push(ranNumber);
+                    
+            }
+       },
+
+       //calculates cookies per hour
+       cookiesPerHour: [],
+       calcCookiesPerHour: function(){
+        for(i=0; i < this.customers.length; i++){
+        this.cookiesPerHour[i] = this.customers[i] * 6.3;
         }
-   }
-   storeHours();
-//to get sales per and hour, ranNumGen() * 6.3
 
-console.log(customers);
-for(i=0; i < customers.length; i++){
-customers[i] =customers[i] * 6.3;
-  
-}
+        },//end function
+        sumCookies: 0,
+        calcTotalCookies: function(){
+            for(i=0; i < this.cookiesPerHour.length; i++){
+                this.sumCookies = this.sumCookies + this.cookiesPerHour[i];
+                
+                }
+              
+        },//end calctotal function
 
-var cookiesPerHour = [0];
+        renderStore: function () {    //function that feeds to renderflavor
+            
+            var list = document.getElementById('ice-cream-list');
+            var newLi = document.createElement('li');
+            newLi.textContent = this.cookiesPerHour[0];  //what will be rendered
+            list.appendChild(newLi);
+            },
+        }, //end of first index or pikes place
+/////////////////////////////////////////////call all the required functions
+        //  pikesPlace.storeHours(),
+        // pikesPlace.calcCookiesPerHour(),
+        // console.log(pikesPlace.cookiesPerHour),
+        // pikesPlace.calcTotalCookies(),
+        // console.log(pikesPlace.sumCookies),
+        // console.log(pikesPlace.cookiesPerHour),
+        
+        
+        
+        // storeName.storeHours(),
+        // storeName.calcCookiesPerHour(),
+        // storeName.calcTotalCookies,
+        // calcTotalCookies(),
+//////////////////////////////////////////////////////////////////end function calls
+
+        ], //end of store array
+            renderStores: function () {
+                for (var i = 0; i < salmonStores.storeName.length; i++) {
+                    salmonStores.storeName[i].renderStore();
+                }
+            }
+
+}    //ends salmonstores
+        
+
+storeName.ranNumGen(),
+console.log(calcCookiesPerHour);
+salmonStores.renderStores() //calls the renderFlavor function to output new li to screen
 
 
 
-function calcCookiesPerHour(){
-    for(i=0; i < customers.length; i++){
-    cookiesPerHour[i] = customers[i] * 6.3;
-    }
-    }//end function
+
+//having trouble getting the functions to instantiate, before I put pikes place in an object I had to 
+//call the functions using pikesPlace.calcCookiesPerHour but I don't know how or where to call inside of object 
+//to make function run.
 
 
-    var sumCookies = 0;
-    for(i=0; i < cookiesPerHour.length; i++){
-    sumCookies = sumCookies + cookiesPerHour[i];
-    }
 
 
-var sumCookies = 0;             //calcs total cookies for day
-function calcTotalCookies(){
 
-for(i=0; i < cookiesPerHour.length; i++){
-sumCookies = sumCookies + cookiesPerHour[i];
 
-}
-}
+
+
+
