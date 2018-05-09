90c1c09ee37a74053d2f863a9e06aa186d4abb48
Jonathan DiQuattro <jondiquattro@hotmail.com>
2018-05-09 08:49:16 -0700

    Revert "back end is working"

--- js/app.js
+++ js/app.js
@@ -1,67 +1,66 @@
 'strict'
 
 var openHours = ['6am', '7am', '8am', '9am','10am','11am','12pm', '1pm', '2pm', '3pm', '4pm','5pm','6pm','7pm','8pm'];
-var stores=[];
+var allStores=[];
 
-function SalmonStores(name, minimumCustomers, maxCustomers, averageCookies ){
-    this.name = name;
-    this.minimumCustomers = minimumCustomers;
-    this.maxCustomers = maxCustomers;
-    this.averageCookies = averageCookies;
-    this.customers = [],      //holds the number of customers for each hour
-    this.cookiesPerHour = [];
-    this.sumCookies = 0;
-    stores.push(this);
+function SalmonStores(){
+    name= '';
+    minimumCustomers = 0;
+    maxCustomers = 0;
+    averageCookies = 0;
+    customers = [],      //holds the number of customers for each hour
+    cookiesPerHour = [];
+    sumCookies = 0;
+    allStores.push(this);
 };
 
+var pikeStore = new SalmonStores('Pikes Place', 23, 65, 6.3);
 
+var seaTac = new SalmonStores('SeaTac Airport', 3, 24, 1.2);
 
-new SalmonStores ('Pikes Place', 23, 65, 6.3);
+var seattleCenter = new SalmonStores('Seattle Center', 11, 38, 3.7);
 
-new SalmonStores('SeaTac Airport', 3, 24, 1.2);
+var capitolHill = new SalmonStores('Capitol Hill', 20, 38, 2.3);
 
-new SalmonStores('Seattle Center', 11, 38, 3.7);
-
-new SalmonStores('Capitol Hill', 20, 38, 2.3);
-
-new SalmonStores('Alki', 2, 16, 4.6);
+var alki = new SalmonStores('Alki', 2, 16, 4.6);
 			
 
 
 
 
 SalmonStores.prototype.ranNumGen=function(){
-    return Math.ceil(Math.random() * ( this.maxCustomers - this.minimumCustomers + 1) + this.minimumCustomers);  //via MDN
+    return Math.ceil(Math.random() * (65 - 23 + 1) + 23);  //via MDN
 };
 
 SalmonStores.prototype.storeHours = function(){
-    for (var i = 0; i <openHours.length; i++){
-// console.log(this.ranNumGen);
+    for (var i = 0; i < 14; i++){
+
         var ranNumber = this.ranNumGen();
-        // console.log(ranNumber);
+        console.log(ranNumber);
         this.customers.push(ranNumber);
                 
         }
 };
 
 SalmonStores.prototype.calcCookiesPerHour = function(){
-    this.storeHours();
-    
-    for(var i=0; i < openHours.length; i++){
-        
+    for(i=0; i < this.customers.length; i++){
         Math.floor(this.cookiesPerHour[i] = this.customers[i] * 6.3);//why not working
-
-
-        this.sumCookies = this.sumCookies + Math.floor(this.cookiesPerHour[i]);
          }
 };
 
+SalmonStores.prototype.calcTotalCookies=function(){
+    this.calcCookiesPerHour
+    for(i=0; i < this.cookiesPerHour.length; i++){
+        this.sumCookies = this.sumCookies + this.cookiesPerHour[i];
+         
+        }
+};
 
 SalmonStores.prototype.render = function() {
 this.calcCookiesPerHour();                 //calls the calcCookiesPerHour
     // grab the parent from the DOM
     var ulEl = document.getElementById('sales')   //creates variable called ulEL and stores html id in it
-    for (var i = 0; i < openHours.length; i++) {       //creates loop of length 14 
+    for (var i = 0; i < this.customers.length; i++) {       //creates loop of length 14 
       // create an element
       var liEl = document.createElement('li');      //creates variable to store li in
       // give the element content
@@ -75,13 +74,7 @@ this.calcCookiesPerHour();                 //calls the calcCookiesPerHour
     ulEl.appendChild(liEl);
 };
 
-function renderAllStores(){
-    for (var i =0; i < stores.length; i++){
-        console.log(i);
-        stores[i].render();
-    }
-};
-renderAllStores();
+
 
 //   var pikesPlace = {
     //     name: 'Pikes Place Market',
