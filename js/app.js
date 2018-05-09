2c80d3a628ae8c208b9c64aaff3329b9c6633dc2
Jonathan DiQuattro <jondiquattro@hotmail.com>
2018-05-09 09:48:42 -0700

    fixed messed up js file

--- js/app.js
+++ js/app.js
@@ -3,68 +3,68 @@
 var openHours = ['6am', '7am', '8am', '9am','10am','11am','12pm', '1pm', '2pm', '3pm', '4pm','5pm','6pm','7pm','8pm'];
 var stores=[];
 
-function SalmonStores(){
-    name= '';
-    minimumCustomers = 0;
-    maxCustomers = 0;
-    averageCookies = 0;
-    customers = [],      //holds the number of customers for each hour
-    cookiesPerHour = [];
-    sumCookies = 0;
+function SalmonStores(name, minimumCustomers, maxCustomers, averageCookies ){
+    this.name = name;
+    this.minimumCustomers = minimumCustomers;
+    this.maxCustomers = maxCustomers;
+    this.averageCookies = averageCookies;
+    this.customers = [],      //holds the number of customers for each hour
+    this.cookiesPerHour = [];
+    this.sumCookies = 0;
     stores.push(this);
 };
 
-var pikeStore = new SalmonStores('Pikes Place', 23, 65, 6.3);
 
-var seaTac = new SalmonStores('SeaTac Airport', 3, 24, 1.2);
 
-var seattleCenter = new SalmonStores('Seattle Center', 11, 38, 3.7);
+new SalmonStores ('Pikes Place', 23, 65, 6.3);
 
-var capitolHill = new SalmonStores('Capitol Hill', 20, 38, 2.3);
+new SalmonStores('SeaTac Airport', 3, 24, 1.2);
 
-var alki = new SalmonStores('Alki', 2, 16, 4.6);
+new SalmonStores('Seattle Center', 11, 38, 3.7);
+
+new SalmonStores('Capitol Hill', 20, 38, 2.3);
+
+new SalmonStores('Alki', 2, 16, 4.6);
 			
 
 
 
 
 SalmonStores.prototype.ranNumGen=function(){
-    return Math.ceil(Math.random() * (65 - 23 + 1) + 23);  //via MDN
+    return Math.ceil(Math.random() * ( this.maxCustomers - this.minimumCustomers + 1) + this.minimumCustomers);  //via MDN
 };
 
 SalmonStores.prototype.storeHours = function(){
-    for (var i = 0; i < 14; i++){
-
+    for (var i = 0; i <openHours.length; i++){
+// console.log(this.ranNumGen);
         var ranNumber = this.ranNumGen();
-        console.log(ranNumber);
+        // console.log(ranNumber);
         this.customers.push(ranNumber);
                 
         }
 };
 
 SalmonStores.prototype.calcCookiesPerHour = function(){
-    for(var i=0; i < this.customers.length; i++){
+    this.storeHours();
+    
+    for(var i=0; i < openHours.length; i++){
+        
         Math.floor(this.cookiesPerHour[i] = this.customers[i] * 6.3);//why not working
+
+
+        this.sumCookies = this.sumCookies + Math.floor(this.cookiesPerHour[i]);
          }
 };
 
-SalmonStores.prototype.calcTotalCookies=function(){
-    this.calcCookiesPerHour
-    for( var i=0; i < this.cookiesPerHour.length; i++){
-        this.sumCookies = this.sumCookies + this.cookiesPerHour[i];
-         
-        }
-};
 
 SalmonStores.prototype.render = function() {
 this.calcCookiesPerHour();                 //calls the calcCookiesPerHour
-
+    
 
 // grab the parent from the DOM
     var teEl = document.getElementById('table');   //creates variable called teEl and stores html id in it
     var trEl = document.createElement('tr'); //creates a table row
     for (var i = 0; i < openHours.length; i++) {       //creates loop of length 14 
-
       // create an element
       var tdEl = document.createElement('td');      //creates 14 table data columns
       // give the element content
@@ -90,7 +90,6 @@ this.calcCookiesPerHour();                 //calls the calcCookiesPerHour
 };
 
 
-//please god dont delete my shit
 
 
 
@@ -104,5 +103,3 @@ function renderAllStores(){
     }
 };
 renderAllStores();
-
-
